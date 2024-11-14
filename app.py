f25349e264d0e1ea3ad90c3ecd814bbc89fc4d20
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2024-11-13 21:32:14 -0800

    used template and updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template
 import requests
 
 app = Flask(__name__)
@@ -16,24 +16,7 @@ def index():
             else:
                 character_data = {'error': 'Character not found.'}  # Add error handling
 
-    return render_template_string('''
-        <form method="post">
-            Character ID: <input type="text" name="character_id">
-            <input type="submit" value="Get Character">
-        </form>
-        {% if character_data %}
-            {% if character_data.error %}
-                <p><strong>Error:</strong> {{ character_data.error }}</p>
-            {% else %}
-                <h2>Character Information</h2>
-                <p><strong>Name:</strong> {{ character_data.name }}</p>
-                <p><strong>Height:</strong> {{ character_data.height }}</p>
-                <p><strong>Mass:</strong> {{ character_data.mass }}</p>
-                <p><strong>Hair Color:</strong> {{ character_data.hair_color }}</p>
-                <p><strong>Eye Color:</strong> {{ character_data.eye_color }}</p>
-            {% endif %}
-        {% endif %}
-    ''', character_data=character_data)
+    return render_template('index.html', character_data=character_data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
